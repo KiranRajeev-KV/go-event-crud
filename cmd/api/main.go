88fb4392124cad3ca4c4b5e71a113904e6b79d75
KiranRajeev-KV <kiranrajeevkv@gmail.com>
2025-08-01 23:15:15 +0530

    feat(api): implement server and routing logic with Gin framework

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,9 +2,19 @@ package main
 
 import (
 	"database/sql"
+	"go-event-crud/internal/database"
+	"go-event-crud/internal/env"
 	"log"
-	_"github.com/mattn/go-sqlite3"
+
+	_ "github.com/mattn/go-sqlite3"
+	_ "github.com/joho/godotenv/autoload"
 )
+
+type application struct {
+	port int
+	jwtSecret string
+	models database.Models
+}
 
 func main() {
 	db,err := sql.Open("sqlite3","./data.db")
@@ -13,4 +23,20 @@ func main() {
 	}
 	
 	defer db.Close()
+
+	// init modals
+	models := database.NewModels(db)
+
+	app  := &application{
+		port: env.GetEnvInt("PORT",6969),
+		jwtSecret: env.GetEnvString("JWT_SECRET","random-secret"),
+		models: models,
+	}
+
+	if err:= app.serve(); err != nil {
+		log.Fatal(err)
+	}
+
+	
+
 }
